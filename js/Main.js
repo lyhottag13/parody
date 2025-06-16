066f56f5026332b22b901e459e350d60c69f88ff
lyhottag13 <leonardo@promoevt.com>
2025-06-15 18:34:49 -0700

    Added Move Bear Function With AD Keys

---
+++ js/Main.js
@@ -0,0 +1,34 @@
+const bear = document.getElementById("bear");
+bear.style.left = "500px";
+
+let holdingLeft = false;
+let holdingRight = false;
+
+document.addEventListener("keydown", action => {
+    switch (action.key) {
+        case "a":
+            holdingLeft = true;
+            break;
+        case "d":
+            holdingRight = true;
+            break;
+    }
+});
+document.addEventListener("keyup", action => {
+    switch (action.key) {
+        case "a":
+            holdingLeft = false;
+            break;
+        case "d":
+            holdingRight = false;
+            break;
+    }
+});
+setInterval(() => {
+    if (holdingLeft) {
+        bear.style.left = (Number.parseInt(bear.style.left.replace("px", "")) - 10) + "px";
+    }
+    if (holdingRight) {
+        bear.style.left = (Number.parseInt(bear.style.left.replace("px", "")) + 10) + "px";
+    }
+}, 20)
