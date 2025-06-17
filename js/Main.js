8b4822488d66abfc6c3d421ca2ebbf405645087f
lyhottag13 <leonardo@promoevt.com>
2025-06-16 19:03:56 -0700

    Added Different Visuals

--- js/Main.js
+++ js/Main.js
@@ -1,73 +1,94 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+const background = document.getElementById("background");
+const backgroundContext = background.getContext("2d");
+
 context.imageSmoothingEnabled = false;
+backgroundContext.imageSmoothingEnabled = false;
 
-const GRAVITY = 0.001;
-const FRICTION = -0.001;
+const GRAVITY = 0.0014;
+const FRICTION = -0.0003;
 
+background.style.background = "white";
 canvas.style.background = "white";
 
 const WINDOW_HEIGHT = window.innerHeight;
 const WINDOW_WIDTH = window.innerWidth;
 
-// canvas.height = WINDOW_HEIGHT;
-// canvas.width = WINDOW_WIDTH;
-
-
-class Sprite {
-    constructor(xpos, ypos, speedX, speedY, spritePath) {
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.speedX = speedX;
-        this.speedY = speedY;
-        this.spritePath = spritePath;
-    }
-    drawImage(context) {
-        context.drawImage(this.spritePath, this.xpos, this.ypos, 32, 32);    
-    }
-    update(delta) {
-        context.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-        this.speedY += GRAVITY * delta;
-        
-        this.xpos += this.speedX * delta;
-        this.ypos += this.speedY * delta;
-        
-        if (this.ypos >= 70) {
-            this.spritePath = image2;
-            this.ypos = 70;
-            this.speedX += FRICTION * delta;
-            if (this.speedX <= 0) {
-                this.speedX = 0;
-            }
-        }
-        
-        this.drawImage(context);
-    }
-    bounce() {
-        this.speedY = -this.speedY * .5;
-    }
-}
+// background.height = WINDOW_HEIGHT;
+// background.width = WINDOW_WIDTH;
 
 const image1 = new Image();
 const image2 = new Image();
+const image3 = new Image();
+const image4 = new Image();
 
+class Sprite {
+    constructor(xpos, ypos, xvel, yvel, image, height, width) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.xvel = xvel;
+        this.yvel = yvel;
+        this.image = image;
+        this.height = height;
+        this.width = width;
+    }
+    update(delta) {
+        context.clearRect(0, 0, 32, 32);
+        context.drawImage(image3, 0, 0);
+        this.yvel += GRAVITY * delta;
+        this.ypos += this.yvel * delta;
+        this.xpos += this.xvel * delta;
+        
+        if (this.ypos >= 300) {
+            context.clearRect(0, 0, 32, 32);
+            context.drawImage(image4, 0, 0);
+            this.ypos = 300;
+            this.xvel += FRICTION * delta;
+            if (this.xvel <= 0) {
+                this.xvel = 0;
+            }
+        }
+        backgroundContext.drawImage(this.image, this.xpos, this.ypos, 128, 128);
+    }
+}
 
-image1.src = "resources/images/flying.png";
-image2.src = "resources/images/floored.png";
+image1.src = "resources/images/guy.png";
+image2.src = "resources/images/background.png";
+image3.src = "resources/images/flying.png";
+image4.src = "resources/images/floored.png";
 
-const sprite = new Sprite(10, 50, 0.09, -0.3, image1);
+let guy;
 
-sprite.drawImage(context);
-
+image1.onload = () => {
+    context.drawImage(image1, 0, 0);
+    guy = new Sprite(40, 130, 0.25, -0.4, canvas, 32, 32);
+}
 let lastTime = 0;
 
+image2.onload = () => {
+    backgroundContext.drawImage(image2, 0, 0, 512, 512);
+    backgroundContext.drawImage(canvas, 40, 130, 128, 128);
+}
+
+let isFirstRun = true;
 function updateCanvas(timestamp) {
+    backgroundContext.clearRect(0, 0, 512, 512);
+    backgroundContext.drawImage(image2, 0, 0, 512, 512);
+    if (isFirstRun) {
+        lastTime = timestamp;
+        isFirstRun = false;
+    }
     const delta = timestamp - lastTime;
     lastTime = timestamp;
 
-    sprite.update(delta);
+    guy.update(delta);
+
     requestAnimationFrame(updateCanvas);
 }
 
-requestAnimationFrame(updateCanvas);
+document.addEventListener("click", () => {
+    requestAnimationFrame(updateCanvas);
+
+})
